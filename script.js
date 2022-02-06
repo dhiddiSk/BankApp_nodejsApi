72fe87292f8e2d9deff29e49a7522a703bc5b0f5
saikrishna <saikrishna.4369@gmail.com>
2022-02-06 20:09:51 +0100

    added dates to transactions

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2,
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
 };
 
+account1.movementsDates.push();
 const account2 = {
   owner: "Jessica Davis",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ]
 };
 
 const account3 = {
@@ -20,6 +41,16 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ]
 };
 
 const account4 = {
@@ -27,11 +58,23 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ]
 };
 
 const accounts = [account1, account2, account3, account4];
 let currentLoggedInUser;
 let sortMovementsState;
+let date;
+
 // FE Elements
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
@@ -57,6 +100,7 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+const loginTimer = document.querySelector(".timer");
 
 // Update the accounts Object with userNames
 
@@ -129,12 +173,19 @@ const updateUserAccountMovements = function (
 
   // deposit or withdrawl
   movs.forEach(function (transaction, index) {
+
+    // Since the array size of movements and dates are same, then use the index of the movements for fetching the date.
+
+    let currentTransactionDate = currentLoggedInUser.movementsDates[index];
+    date = new Date(currentTransactionDate);
+
     const type = transaction > 0 ? "deposit" : "withdrawal";
     const htmlElement = `
      <div class="movements__row">
      <div class="movements__type movements__type--${type}">${
       index + 1
     }${type}</div>
+     <div class="movements__date">${date.getDate()}\\${date.getMonth()}\\${date.getFullYear()}</div>
      <div class="movements__value">${transaction}€</div>
    </div>
    `;
@@ -159,6 +210,7 @@ const transferMoney = function () {
     reciverAccount.movements.push(Number(inputTransferAmount.value));
   }
   currentLoggedInUser.movements.push(-Number(inputTransferAmount.value));
+  currentLoggedInUser.movementsDates.push(new Date().toISOString());
   updateUserAccountMovements(currentLoggedInUser);
   inputTransferTo.value = "";
   inputTransferAmount.value = "";
@@ -166,13 +218,14 @@ const transferMoney = function () {
 
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
-  transferMoney();
+  transferMoney(); 
 });
 
 //Request money functionality
 const requestMoney = function () {
   if (Number(inputLoanAmount.value) > 0) {
     currentLoggedInUser.movements.push(Number(inputLoanAmount.value));
+    currentLoggedInUser.movementsDates.push(new Date().toISOString());
     updateUserAccountMovements(currentLoggedInUser);
     inputLoanAmount.value = "";
   }
@@ -181,7 +234,13 @@ const requestMoney = function () {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
   requestMoney();
+  currentLoggedInUser.movementsDates.push(date.toISOString());
 });
+
+const loginTimer = function(){
+
+
+};
 
 // Login implementation of the user
 btnLogin.addEventListener("click", function (e) {
@@ -196,6 +255,7 @@ btnLogin.addEventListener("click", function (e) {
 
     // Now user is allowed to view his/her account
     document.querySelector(".app").style.opacity = 100;
+    loginTimer();
 
     inputLoginUsername.value = "";
     inputLoginPin.value = "";
@@ -210,3 +270,4 @@ btnSort.addEventListener("click", function (e) {
   updateUserAccountMovements(currentLoggedInUser, !sortMovementsState);
   sortMovementsState = !sortMovementsState;
 });
+
