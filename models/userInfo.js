979379bf9f1079e0464d48783ea5b2174221642b
saikrishna <saikrishna.4369@gmail.com>
2022-08-18 23:53:56 +0200

    starting with DB schemas

---
+++ models/userInfo.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+// Basic schema for the user registration
+const userRegisterSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const userBankAccountSchema = new Schema({
+
+    accountNumber,
+    accountType,
+    currentAmount  
+    
+});
+
+
+const userTransactionsSchema = new Schema({
+    transactions
+});
+
+
+
+
+
+
+
+const UserReg = mongoose.model("newUserReg", userRegisterSchema);
+
+export default UserReg;
