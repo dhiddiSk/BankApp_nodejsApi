f8a5884b7c30eeb61dd67e772305e3d57762e5c6
saikrishna <saikrishna.4369@gmail.com>
2022-10-10 22:54:57 +0200

    update

---
+++ routes/Auth.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+//new user registration.
+app.post('/register', function(req, res, next){
+
+});
+
+//user login.
+app.post('/login', function(req, res, next){
+
+});
+
+//user transactions.
+app.get('/transactions', function(req, res, next){
+
+});
+
+//user profile
+app.get('/profile', function(req, res, next){
+
+});
