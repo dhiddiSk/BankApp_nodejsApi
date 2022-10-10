4157035878140ac3117a69eab59e3d9c5c2c5e7b
saikrishna <saikrishna.4369@gmail.com>
2022-10-10 22:56:15 +0200

    update

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var app = express();
+import newUserReg from "../models/userInfo.js";
+import express from "express";
+import bycrypt from "bcryptjs";
+import jsonwt from "jsonwebtoken";
+
+const express = require('express');
+const app = express();
+
+const registration = async function (req, res) {
+    const newUser = new newUserReg({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      userName: req.body.userName
+    });
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(newUser.password, salt);
+    newUser.password = hashpassword;
+    const userSignup = await newUser.save();
+  
+    const payloadForJwt = {
+      id: userSignup.id,
+      name: userSignup.name,
+      email: userSignup.email,
+    };
+  
+    const jwtToken = await jwtTokenGeneration(payloadForJwt);
+    res.status(200).json({
+      success: true,
+      token: jwtToken,
+    });
+  };
 
 //new user registration.
 app.post('/register', function(req, res, next){
-
+    registration(req, res);
 });
 
 //user login.
