343ffc4c1de8f42103640764ce55cecf1c87597e
saikrishna <saikrishna.4369@gmail.com>
2022-10-10 22:55:50 +0200

    added basic user bank schema

--- models/userInfo.js
+++ models/userInfo.js
@@ -1,54 +1,58 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+// As per the requirement of the project, it is understood that embedded data model is most suitable for this project
 
 // Basic schema for the user registration
 const userRegisterSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    maxLength: 100,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-    maxLength: 100,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-const userBankAccountSchema = new Schema({
-    accountNumber: {
-      type: Number,
+  userRegisterData: {
+    name: {
+      type: String,
+      required: true,
+      maxLength: 50
+    },
+    email: {
+      type: String,
       required: true
-    }, 
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    userName: {
+      type: String,
+      required: true,
+      maxLength: 100
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+  },
+  userBankData: {
+    accountNumber: {
+      type: String,
+      default: "XXXX"
+    },
     accountType: {
       type: String,
       default: "savings"
-    }, 
+    },
     currentAmount: {
       type: Number,
       default: 0
-    }
+    },
+  },
+  transactions: {
+    sent: [{
+      type: Number
+    }],
+    recieved: [{
+      type: Number
+    }]
+  },
 });
 
-const userTransactionsSchema = new Schema({
-    transactions : {
-      sending : [0],
-      recieving: [0]
-    }
-});
+const userReg = mongoose.model("usersBankData", userRegisterSchema);
 
-const userReg = mongoose.model("newUserReg", userRegisterSchema);
-
-export default UserReg;
+export default userReg;
