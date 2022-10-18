bc147e64283fa3bb09c01867d55d46ffc81c18cc
saikrishna <saikrishna.4369@gmail.com>
2022-10-18 23:46:02 +0200

    added user login validation

--- routes/Auth.js
+++ routes/Auth.js
@@ -42,6 +42,54 @@ app.post('/register', function(req, res, next){
 
 //user login.
 app.post('/login', function(req, res, next){
+/*
+ Here, the logic goes is to check first of all if the email exists in the database. If exists,
+ then check for the compatability of the password.
+ 
+*/
+// @type    POST
+// @route    /api/auth/login
+// @desc    route for login of registered users
+// @access  PUBLIC
+
+router.post("/login", (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  userReg.findOne({ email })
+    .then((user) => {
+      if (user) {
+        bcrypt
+          .compare(password, user.password)
+          .then((correctPassword) => {
+            if (!correctPassword) {
+              return res.status(401).json({ message: "User login failure" });
+            }
+
+            const payload = {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            };
+
+            // Generate jwt token and send it back to client
+            jsonwt.sign(payload, secret, { expiresIn: 3600 }, (err, token) => {
+              res.json({
+                success: true,
+                token: token,
+              });
+            });
+          })
+          .catch((error) => {
+            console.log(`Error with passwords: ${error}`);
+          });
+      } else {
+        return res.status(404).json({ message: "Email doesn't exists" });
+      }
+    })
+    .catch((error) => {
+      console.log(`Error while login: ${error}`);
+    });
+});
 
 });
 
