04940299be137bb7ffeb0a440be19e7ac57e5219
saikrishna <saikrishna.4369@gmail.com>
2022-11-22 12:34:15 +0100

    restructured the project

--- controllers/userAccount.js
+++
@@ -1,8 +0,0 @@
-import { v4 as uuidv4 } from 'uuid'
-
-const userAccountNumberGeneration = function () {
-  const userAccounUUID = uuidv4()
-  return userAccounUUID
-}
-
-export { userAccountNumberGeneration }
--- controllers/userAccountTransactions.js
+++
@@ -1,8 +0,0 @@
-/*
- This module should fetch the latest data of the user transactions from the DB and forward
- it to the frontend.
-*/
-
-const userTransactions = function (userDetails) {}
-
-export { userTransactions }
